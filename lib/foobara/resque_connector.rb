4030349282b8df91c2f0af4e8aa054a6f63a3234
Miles Georgi <azimux@gmail.com>
2024-02-02 22:36:59 +0000

    Initial commit, generated from RubyMine

---
+++ lib/foobara/resque_connector.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "resque_connector/version"
+
+module Foobara
+  module ResqueConnector
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/foobara/resque_connector/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Foobara
+  module ResqueConnector
+    VERSION = "0.1.0"
+  end
+end
