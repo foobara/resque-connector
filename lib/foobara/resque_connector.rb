48311bb1723fbe5f776b9ae9706ad578d5ea34b5
Miles Georgi <azimux@gmail.com>
2024-02-03 01:25:12 +0000

    Clear out redis database after each spec example

--- lib/foobara/resque_connector.rb
+++ lib/foobara/resque_connector.rb
@@ -1,3 +1,16 @@
 require "resque"
 
-Foobara::Util.require_directory "#{__dir__}/../../src"
+require "foobara/all"
+
+module Foobara
+  module ResqueConnector
+    class << self
+      def reset_all
+        # TODO: protect against this in production
+        Resque.redis.flushdb
+      end
+    end
+  end
+end
+
+Foobara::Util.require_directory("#{__dir__}/../../src")
